b5b53e031e6cf141e6e36cbd99554832d40cbca7
Parth Waghmare <waghmareparth11@gmail.com>
2019-04-23 13:25:20 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'menu', loadChildren: './menu/menu.module#MenuPageModule' },
+  { path: 'payment', loadChildren: './payment/payment.module#PaymentPageModule' },
+  { path: 'confirmation', loadChildren: './confirmation/confirmation.module#ConfirmationPageModule' },
+  { path: 'group', loadChildren: './group/group.module#GroupPageModule' },
+  { path: 'account-info', loadChildren: './account-info/account-info.module#AccountInfoPageModule' }
 ];
 @NgModule({
   imports: [
